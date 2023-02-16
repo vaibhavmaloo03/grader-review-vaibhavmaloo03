e603f58ef5d5e4245f840c1051b148c22b3b1ca1
Jeffrey Hata <67081225+Jeff591@users.noreply.github.com>
2023-02-16 14:30:01 -0800

    Changes in Lab

--- grade.sh
+++ grade.sh
@@ -3,3 +3,31 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -f "ListExamples.java" ]]
+then
+    echo "ListExamples found"
+else 
+    echo "need file ListExamples.java"
+    exit 1
+fi
+
+cp ../TestListExamples.java ./
+cp ../Server.java ./
+cp ../GradeServer.java ./
+
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar TestListExamples.java ListExamples.java
+if [[ -f "ListExamples.class"]] && [[ -f "TestListExamples.class"]]
+then
+    echo "Compiled files"
+else
+    echo "Files did not compile correctly"
+    exit 1
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
